ca79ad79c7884099d883e41fe3b4aaa1889e15f5
torilin <torilinsf@gmail.com>
2016-07-24 15:10:48 -0700

    Linked up Model + View

--- tapperapp/ViewController.swift
+++ tapperapp/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    @IBOutlet weak var tapperLogo: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var tapButton: UIButton!
+    
+    @IBOutlet weak var coinButton: UIButton!
+    @IBOutlet weak var tapsLabel: UILabel!
+    
+    
 
 }
 
