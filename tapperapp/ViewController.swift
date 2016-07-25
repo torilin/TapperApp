76e1bca79bcee1925d8015f426976eb24c9afcca
torilin <torilinsf@gmail.com>
2016-07-24 19:49:05 -0700

    Tapping Gaming App. Working Production.

--- tapperapp/ViewController.swift
+++ tapperapp/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Properties
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
+    
+    
+    
+    
+    //Oulets
     @IBOutlet weak var tapperLogo: UIImageView!
     @IBOutlet weak var howManyTapsTxt: UITextField!
     @IBOutlet weak var tapButton: UIButton!
@@ -17,7 +24,61 @@ class ViewController: UIViewController {
     @IBOutlet weak var coinButton: UIButton!
     @IBOutlet weak var tapsLabel: UILabel!
     
+    @IBAction func coinButtonPressed(sender: UIButton!) {
+        currentTaps += 1
+        
+        updateTabsLabel()
+        
+        if isGameOver() {
+            restartGame()
+        }
+    }
     
+    @IBAction func playButtonPressed(sender: UIButton!) {
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            
+            tapperLogo.hidden = true
+            howManyTapsTxt.hidden = true
+            tapButton.hidden = true
+            
+            coinButton.hidden = false
+            tapsLabel.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            updateTabsLabel()
+            
+        }
 
+    }
+    
+    func restartGame () {
+        
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        tapperLogo.hidden = false
+        howManyTapsTxt.hidden = false
+        tapButton.hidden = false
+        
+        coinButton.hidden = true
+        tapsLabel.hidden = true
+        
+    }
+    
+    func isGameOver () -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func updateTabsLabel() {
+        tapsLabel.text = "\(currentTaps) Taps"
+    }
+    
 }
 
